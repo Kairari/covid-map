1a8da7709d6e59041a3a369fe6ed4f1f428689ae
Your Name <you@example.com>
2020-04-07 12:11:45 -0400

    Added some more countries and links.

--- public/js/map.js
+++ public/js/map.js
@@ -17,59 +17,216 @@ $("#container").mapael({
         area: {
             title: "COVID Information",
             slices: [
+              // Green shades, from
+              // https://i.pinimg.com/originals/f4/c0/28/f4c0289ea63f9f640072b5e5c1cf6fc8.png
                 {
-                    max: 100000,
+                    max: -9,
+                    attrs: {
+                        fill: "#006a4e"
+                    },
+                    label: "Less than -9"
+                },
+                {
+                    min: -9,
+                    max:-5,
+                    attrs: {
+                        fill: "#5ca08e"
+                    },
+                    label: "Between -5 and -9"
+                },
+                {
+                    min: -5,
+                    max:-2,
+                    attrs: {
+                        fill: "#8abaae"
+                    },
+                    label: "Between -5 and -9"
+                },
+                {
+                    min:-2,
+                    max: 0,
+                    attrs: {
+                        fill: "#b8d5cd"
+                    },
+                    label: "Between -2 and 0"
+                },
+                {
+                    min: 0,
+                    max: 2,
                     attrs: {
                         fill: "#F5A29F"
                     },
-                    label: "Less than 100,000"
+                    label: "Less than 2"
                 },
                 {
-                    min: 100000,
-                    max: 500000,
+                    min: 2,
+                    max: 5,
                     attrs: {
                         fill: "#FF514D"
                     },
-                    label: "Between 100,000 and 500,000"
+                    label: "Between 2 and 5"
                 },
                 {
-                    min: 500000,
-                    max: 1000000,
+                    min: 5,
+                    max: 9,
                     attrs: {
                         fill: "#800300"
                     },
-                    label: "Between 500,000 and 1,000,000"
+                    label: "Between 5 and 9"
                 },
                 {
-                    min: 1000000,
+                    min: 9.01,
+                    max: 9.99,
+                    attrs: {
+                        fill: "#580000"
+                    },
+                    label: "Between 9 and 10"
+                },
+                {
+                    min: 10,
                     attrs: {
                         fill: "#000000"
                     },
-                    label: "Over 1,000,000"
+                    label: "Over 10"
                 }
             ]
         }
     },
     areas: {
-        "NZ": {
-            value: 421234,
-            href: "#",
-        },
+        // Philippines
         "PH": {
-            value: 542910,
+            value: 10,
             href: "#",
         },
+        // Australia
+        "AU": {
+            value: 0,
+            href: "#",
+        },
+        // Egypt
+        "EG": {
+            value: 9,
+            href: "https://en.wikipedia.org/wiki/Egypt"
+        },
+        // China
         "CN": {
-            value: 100000000,
-            href: "#"
+            value: -3,
+            href: "https://en.wikipedia.org/wiki/China"
         },
+        // Japan
+        "JP": {
+            value: 3,
+            href: "https://en.wikipedia.org/wiki/Japan"
+        },
+        // South Korea
+        "KR": {
+            value: -10,
+            href: "https://en.wikipedia.org/wiki/South_Korea"
+        },
+        // India
+        "IN": {
+            value: 9.5,
+            href: "https://en.wikipedia.org/wiki/India"
+        },
+        // Mongolia
+        "MN": {
+            value: 0,
+            href: "https://en.wikipedia.org/wiki/India"
+        },
+        // Kazakhstan
+        "KZ": {
+            value: 2,
+            href: "https://en.wikipedia.org/wiki/India"
+        },
+        // Germany
+        "DE": {
+            value: 1,
+            href: "https://en.wikipedia.org/wiki/India"
+        },
+        // Kenya
+        "KE": {
+            value: 4,
+            href: "https://en.wikipedia.org/wiki/India"
+        },
+        // Indonesia
+        "ID": {
+            value: 9,
+            href: "https://en.wikipedia.org/wiki/Indonesia"
+        },
+        // Iraq
+        "IQ": {
+            value: 9,
+            href: "https://en.wikipedia.org/wiki/Indonesia"
+        },
+        // Iran
+        "IR": {
+            value: 4,
+            href: "https://en.wikipedia.org/wiki/Indonesia"
+        },
+        // Pakistan
+        "PK": {
+            value: 10,
+            href: "https://en.wikipedia.org/wiki/Indonesia"
+        },
+        // Italy
+        "IT": {
+            value: 4,
+            href: "https://en.wikipedia.org/wiki/Indonesia"
+        },
+        // Bangladesh
+        "BD": {
+            value: 10,
+            href: "https://en.wikipedia.org/wiki/Bangladesh"
+        },
+        // Libya
+        "LB": {
+            value: 4,
+            href: "https://en.wikipedia.org/wiki/Libya"
+        },
+        // United States USA
         "US": {
-            value: 900,
+            value: 3,
             href: "#"
         },
-        "BR": {
-            value: "555555",
+        // France
+        "FR": {
+            value: 3,
             href: "#"
-        }
+        },
+        // Brazil
+        "BR": {
+            value: 3,
+            href: "#"
+        },
+        // Vietnam
+        "VN": {
+            value: 3,
+            href: "http://www.google.com",
+        },
+        // Taiwan
+        "TW": {
+            value: -6,
+            href: "http://www.google.com",
+        },
+        // Argentina
+        "AR": {
+            value: 7,
+            href: "http://www.google.com",
+        },
+        // Afghanistan
+        "AF": {
+            value: 2,
+            href: "http://www.google.com",
+        },
+        // Colombia
+        "CO": {
+            value: 7,
+            href: "http://www.google.com",
+        },
+        // New Zealand
+        "NZ": {
+            value: 1,
+            href: "www.google.com",
+        },
     }
 });
